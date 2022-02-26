8ef7552449e9e793ec65f0317d30074668259c85
Umar Mohammed <umar.mohammed@holisticai.com>
2022-02-26 14:35:08 +0000

    first commit

---
+++ src/app/customers/selected-customer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <!-- 3. TODO Display the properties of the selected customer -->
+    <ul>
+      <li>Customer Id:</li>
+      <li>Customer Name:</li>
+      <li>Customer Address:</li>
+      <li>Customer City:</li>
+      <li>Customer Country:</li>
+    </ul>
+  `,
+})
+export class SelectedCustomerPageComponent {}
